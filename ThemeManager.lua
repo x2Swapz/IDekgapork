8fe618c2b9af79230009cee0e0168a47c279ef7a
x.09z <98596037+x2Swapz@users.noreply.github.com>
2023-02-16 22:43:05 +0700

    Update ThemeManager.lua

--- ThemeManager.lua
+++ ThemeManager.lua
@@ -88,17 +88,26 @@ local ThemeManager = {} do
 		end
 
 		table.sort(ThemesArray, function(a, b) return self.BuiltInThemes[a][1] < self.BuiltInThemes[b][1] end)
-                
-		groupbox:AddButton('Default Theme', function()
-	           self.Library.FontColor = (255, 255, 255)
-		self.Library.MainColor = (0, 14, 30)
-		self.Library.AccentColor = (0, 225, 242)
-		self.Library.BackgroundColor = (5, 2, 21)
-		self.Library.OutlineColor = (0, 0, 6)
+               
+                groupbox:AddToggle('Keybind', {
+                Text = 'Show Keybinds Menu',
+                local Default = false, -- Default value (true / false)
+                })
+		
+                Toggles.Keybind:OnChanged(function()
+                Library:SetWatermarkVisibility(Default)
+                end)
 
-		self.Library.AccentColorDark = self.Library:GetDarkerColor(self.Library.AccentColor);
-		self.Library:UpdateColorsUsingRegistry()
-		end)
+		groupbox:AddToggle('WaterMark', {
+                Text = 'Show Watermark',
+                local Default = false, -- Default value (true / false)
+                })
+		
+                Toggles.WaterMark:OnChanged(function()
+                Library.KeybindFrame.Visible = Default;
+                end)
+		
+		groupbox:AddButton('Unload', function() Library:Unload() end)
 		
 		groupbox:AddDropdown('ThemeManager_ThemeList', { Text = 'Theme list', Values = ThemesArray, Default = 1 })
 
