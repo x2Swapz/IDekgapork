6270db1bfe07063c86cea5275102a9801cd5ba83
x.09z <98596037+x2Swapz@users.noreply.github.com>
2023-02-16 22:46:56 +0700

    Update and rename TM.lua to Tmx.lua

--- Tmx.lua
+++ Tmx.lua
@@ -89,7 +89,6 @@ local ThemeManager = {} do
 
 		table.sort(ThemesArray, function(a, b) return self.BuiltInThemes[a][1] < self.BuiltInThemes[b][1] end)
 
-		groupbox:AddDivider()
 		groupbox:AddDropdown('ThemeManager_ThemeList', { Text = 'Theme list', Values = ThemesArray, Default = 1 })
 
 		groupbox:AddButton('Set as default', function()
